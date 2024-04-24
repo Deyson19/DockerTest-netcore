2ce8e481ed3d29a70daede4b0b385a759e3c2e17
Deyson Venté <deiandresvent@hotmail.com>
2024-04-24 11:10:38 -0500

    todo funciona

--- obj/Debug/net8.0/DockerTest.AssemblyInfo.cs
+++ obj/Debug/net8.0/DockerTest.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DockerTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4444633867cd28ea62078a05f10a971ea76ec085")]
 [assembly: System.Reflection.AssemblyProductAttribute("DockerTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DockerTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
